c2f4cf3364895a47b550f60f388660adc01e00b4
Sebastián García Martínez <sebastiangarcia_m@outlook.com>
2022-03-02 09:26:49 +0100

    scrape is WIP and save to csv is almost ready

--- sebas.py
+++ sebas.py
@@ -2,182 +2,11 @@ import requests
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
- 
-url = "https://www.immoweb.be/en/classified/house/for-sale/merelbeke/9820/9768055?searchId=621c8a1b690a0"
-r = requests.get(url)
-
-soup = BeautifulSoup(r.text, "html.parser")
-table = soup.findAll("table", {"class": "classified-table"})
-
-type_property = (
-            soup.find("h1", {"class": "classified__title"})
-            .text.strip()
-            .replace("\n", "")
-            .split()[0]
-        )
-
-subtype_property = type_property
-
-price = soup.find("span", {"class": "sr-only"}).text.strip().replace("€", "")
-
-price = "None" if price == None else price
-
- # We limit type_property to house or appartment
-if type_property.lower() not in ["house", "appartment"]:
-        type_property = "House"
-
-headers_df = [
-        "Locality",
-        "Type of property",
-        "Subtype of property",
-        "Price",
-        "Type of sale",
-        "Number of rooms",
-        "Area",
-        "Fully equipped kitchen",
-        "Furnished",
-        "Open fire",
-        "Terrace",
-        "Garden",
-        "Surface of the land",
-        "Surface area of the plot of land",
-        "Number of facades",
-        "Swimming pool",
-        "State of the building",
-    ]
-
-df = pd.DataFrame(columns=headers_df)
-
-    # dictionary containing the scraped data
-my_dict = {}
-for table_item in table:
-
-        for row in table_item.find_all("tr", class_="classified-table__row"):
-
-            if row is not None:
-
-                header_row = row.find(
-                    "th", class_="classified-table__header", text=True
-                )
-                data_row = row.find("td", class_="classified-table__data", text=True)
-
-                if header_row is not None and data_row is not None:
-                    header_name = str(header_row.string).strip()
-                    column_name = str(data_row.string).strip()
-
-                    my_dict[header_name] = column_name
-
-locality = (
-        "None"
-        if my_dict["Neighbourhood or locality"] == None
-        else my_dict["Neighbourhood or locality"]
-    )
-type_sale = (
-        "None" if my_dict["Tenement building"] == None else my_dict["Tenement building"]
-    )
-numbers_rooms = "None" if my_dict["Bedrooms"] == None else my_dict["Bedrooms"]
-area = ""
-if my_dict["Kitchen type"] == "Installed":
-
-        kitchen = 1
-else:
-
-        kitchen = 0
-
-furnished = "None" if my_dict["Furnished"] == None else my_dict["Furnished"]
-    # No info on immoweb about that
-open_fire = ""
-    # No confirmation on garden on the page but it does have garden surface, so maybe we could use that to confirm that garden exists
-garden = ""
-surface_of_land = ""
-terrace = "None" if my_dict["Terrace"] == None else my_dict["Terrace"]
-surface_plot_land = ""
-number_facades = (
-        "None"
-        if my_dict["Number of frontages"] == None
-        else my_dict["Number of frontages"]
-    )
-swimming_pool = ""
-state_building = (
-        "None"
-        if my_dict["Building condition"] == None
-        else my_dict["Building condition"]
-    )
-
-my_list = [
-        locality,
-        type_property,
-        subtype_property,
-        price,
-        type_sale,
-        numbers_rooms,
-        area,
-        kitchen,
-        furnished,
-        open_fire,
-        terrace,
-        garden,
-        surface_of_land,
-        surface_plot_land,
-        number_facades,
-        swimming_pool,
-        state_building,
-    ]
-
-    # Create new dictionary from headers_df and my_list
-new_row = dict(zip(headers_df, my_list))
-
-df = df.append(new_row, ignore_index=True)
-    # Replaces Yes and No for 1/0
-
-df["Furnished"] = df["Furnished"].replace({"No": 0, "Yes": 1}).astype(int)
-df["Terrace"] = df["Terrace"].replace({"No": 0, "Yes": 1}).astype(int)
-
-    # Convert all empty strings to None
-df = df.replace(r"^\s*$", "None", regex=True)
-df.head()
-print(df.head())
-
-    # return df
-
-
-"""
-# list of urls to test
-
-my_url_list = [
-    "https://www.immoweb.be/en/classified/house/for-sale/merelbeke/9820/9768055?searchId=621c8a1b690a0",
-    "https://www.immoweb.be/en/classified/apartment-block/for-sale/aarschot/3200/9781041?searchId=621e8012c1135",
-    "https://www.immoweb.be/en/classified/house/for-sale/de-panne/8660/9781188?searchId=621e8012c1135",
-    "https://www.immoweb.be/en/classified/house/for-sale/brasschaat/2930/9781340?searchId=621e8012c1135",
-    "https://www.immoweb.be/en/classified/house/for-sale/hoogstraten/2320/9780998?searchId=621e81bd33bc2",
-]
-
+#Work in progress: get rid of NaN, turn yes and no to 1/0 and try to get the missing data, Locality also not showing, like area, swimming pool..
+#Note: Some data is set as default empty because for now I haven't found a way to get that data, I will try..
 
 def getDataFrame(my_urls):
-    for my_url in my_urls:
-        url = my_url
-        r = requests.get(url)
-
-        soup = BeautifulSoup(r.text, "html.parser")
-        table = soup.findAll("table", {"class": "classified-table"})
-
-        type_property = (
-            soup.find("h1", {"class": "classified__title"})
-            .text.strip()
-            .replace("\n", "")
-            .split()[0]
-        )
-
-    subtype_property = type_property
-
-    price = soup.find("span", {"class": "sr-only"}).text.strip().replace("€", "")
-
-    price = "None" if price == None else price
-
-    # We limit type_property to house or appartment
-    if type_property.lower() not in ["house", "appartment"]:
-        type_property = "House"
-
+    
     headers_df = [
         "Locality",
         "Type of property",
@@ -196,67 +25,77 @@ def getDataFrame(my_urls):
         "Number of facades",
         "Swimming pool",
         "State of the building",
-    ]
+        ]
 
     df = pd.DataFrame(columns=headers_df)
+    
+    for my_url in my_urls:
+        url = my_url
+        r = requests.get(url)
 
-    # dictionary containing the scraped data
-    my_dict = {}
-    for table_item in table:
+        soup = BeautifulSoup(r.text, "html.parser")
+        table = soup.findAll("table", {"class": "classified-table"})
 
-        for row in table_item.find_all("tr", class_="classified-table__row"):
+        type_property = (
+            soup.find("h1", {"class": "classified__title"})
+            .text.strip()
+            .replace("\n", "")
+            .split()[0]
+        )
 
-            if row is not None:
+        subtype_property = type_property
 
-                header_row = row.find(
+        price = soup.find("span", {"class": "sr-only"}).text.strip().replace("€", "")
+
+        price = "None" if price == None else price
+
+        # We limit type_property to house or appartment
+        if type_property.lower() not in ["house", "appartment"]:
+            type_property = "House"
+
+        # dictionary containing the scraped data
+        my_dict = {}
+        for table_item in table:
+
+            for row in table_item.find_all("tr", class_="classified-table__row"):
+
+                if row is not None:
+
+                    header_row = row.find(
                     "th", class_="classified-table__header", text=True
-                )
-                data_row = row.find("td", class_="classified-table__data", text=True)
+                    )
+                    data_row = row.find("td", class_="classified-table__data", text=True)
 
-                if header_row is not None and data_row is not None:
-                    header_name = str(header_row.string).strip()
-                    column_name = str(data_row.string).strip()
+                    if header_row is not None and data_row is not None:
+                        header_name = str(header_row.string).strip()
+                        column_name = str(data_row.string).strip()
 
-                    my_dict[header_name] = column_name
+                        my_dict[header_name] = column_name
+    
+        locality = my_dict.get("Neighbourhood or locality")
+        type_sale= my_dict.get("Tenement building")
+        numbers_rooms = my_dict.get("Bedrooms")
+        area = ""
+    
+        if my_dict.get("Kitchen type") == "Installed":
 
-    locality = (
-        "None"
-        if my_dict["Neighbourhood or locality"] == None
-        else my_dict["Neighbourhood or locality"]
-    )
-    type_sale = (
-        "None" if my_dict["Tenement building"] == None else my_dict["Tenement building"]
-    )
-    numbers_rooms = "None" if my_dict["Bedrooms"] == None else my_dict["Bedrooms"]
-    area = ""
-    if my_dict["Kitchen type"] == "Installed":
+            kitchen = 1
+        else:
 
-        kitchen = 1
-    else:
+            kitchen = 0
 
-        kitchen = 0
-
-    furnished = "None" if my_dict["Furnished"] == None else my_dict["Furnished"]
-    # No info on immoweb about that
-    open_fire = ""
-    # No confirmation on garden on the page but it does have garden surface, so maybe we could use that to confirm that garden exists
-    garden = ""
-    surface_of_land = ""
-    terrace = "None" if my_dict["Terrace"] == None else my_dict["Terrace"]
-    surface_plot_land = ""
-    number_facades = (
-        "None"
-        if my_dict["Number of frontages"] == None
-        else my_dict["Number of frontages"]
-    )
-    swimming_pool = ""
-    state_building = (
-        "None"
-        if my_dict["Building condition"] == None
-        else my_dict["Building condition"]
-    )
-
-    my_list = [
+        furnished = my_dict.get("Furnished")
+        # No info on immoweb about that
+        open_fire = ""
+        # No confirmation on garden on the page but it does have garden surface, so maybe we could use that to confirm that garden exists
+        garden = ""
+        surface_of_land = ""
+        terrace = my_dict.get("Terrace")
+        surface_plot_land = ""
+        number_facades = my_dict.get("Number of frontages")
+        swimming_pool = ""
+        state_building = my_dict.get("Building condition")
+        my_list = [
         locality,
         type_property,
         subtype_property,
@@ -274,24 +113,42 @@ def getDataFrame(my_urls):
         number_facades,
         swimming_pool,
         state_building,
-    ]
+        ]
 
-    # Create new dictionary from headers_df and my_list
-    new_row = dict(zip(headers_df, my_list))
+        # Create new dictionary from headers_df and my_list
+        new_row = dict(zip(headers_df, my_list))
 
-    df = df.append(new_row, ignore_index=True)
-    # Replaces Yes and No for 1/0
+        df = df.append(new_row, ignore_index=True)
+        # Replaces Yes and No for 1/0, was working before but not now
 
-    df["Furnished"] = df["Furnished"].replace({"No": 0, "Yes": 1}).astype(int)
-    df["Terrace"] = df["Terrace"].replace({"No": 0, "Yes": 1}).astype(int)
+        #df["Furnished"] = df["Furnished"].replace({"No": 0, "Yes": 1}).astype(int)
+        #df["Terrace"] = df["Terrace"].replace({"No": 0, "Yes": 1}).astype(int)
 
-    # Convert all empty strings to None
-    df = df.replace(r"^\s*$", "None", regex=True)
+        # Convert all empty strings to None
+        df = df.replace(r"^\s*$", "None", regex=True)
+    
+    df
     df.head()
-    print(df.head())
+       # print(df.head())
 
-    # return df
+    return df
+    # list of urls to test
 
+my_url_list = [
+    "https://www.immoweb.be/en/classified/house/for-sale/merelbeke/9820/9768055?searchId=621c8a1b690a0",
+    "https://www.immoweb.be/en/classified/apartment-block/for-sale/aarschot/3200/9781041?searchId=621e8012c1135",
+    "https://www.immoweb.be/en/classified/house/for-sale/de-panne/8660/9781188?searchId=621e8012c1135",
+    "https://www.immoweb.be/en/classified/house/for-sale/brasschaat/2930/9781340?searchId=621e8012c1135",
+    "https://www.immoweb.be/en/classified/house/for-sale/hoogstraten/2320/9780998?searchId=621e81bd33bc2",
+]
 
-getDataFrame()
-"""
+df = getDataFrame(my_url_list)
+
+def saveTocsv(myDataFrame):
+    
+    myDataFrame.to_csv('properties.csv', index=False)
+    
+saveTocsv(df)
+
+df_saved_file = pd.read_csv('properties.csv')
+print(df_saved_file)
